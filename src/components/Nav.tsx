21c0210b5cb2eb4c86f81787fb5e36a567bfb6d4
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-04-30 20:27:29 -0400

    Created the store and connected it to the UI

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-
+import { useCartStore } from "../store";
+import cartIcon from "../assets/cart.svg";
 import "../App.css";
 
 export const Nav = () => {
-
-
+    const cartItems = useCartStore((state) => state.cartItems);
 
     return (
         <nav className="nav-container">
@@ -13,7 +13,12 @@ export const Nav = () => {
             <Link to="/">Products</Link>
             </li>
             <li className="nav-item">
-
+            <Link to="/cart" className="cart-link">
+                {cartItems.length > 0 && (
+                <span className="cart-items-count">{cartItems.length}</span>
+                )}
+                <img src={cartIcon} alt="cart link" className="cart-icon" />
+            </Link>
             </li>
         </ul>
         </nav>
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,11 +1,50 @@
-
+import { Link } from "react-router-dom";
+import { formatCurrency, isInCart } from "../helpers";
+import { products } from "../Data";
+import { Product } from "../types";
+import { useCartStore } from "../store";
+import { useEffect, useState } from "react";
 
 export const ProductsList = () => {
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
+    const cartItems = useCartStore((state) => state.cartItems);
+    const addItem = useCartStore((state) => state.addItem);
 
+    useEffect(() => {
+        const fetchProducts = async () => {
+        // normally we would fetch products from an API
+        const selectedProducts = await Promise.resolve(products);
+        setSelectedProducts(selectedProducts);
+        };
+        fetchProducts();
+    }, []);
+
+    const handleAddToCart = (product: Product) => {
+        addItem({ product, quantity: 1 });
+    };
 
     return (
         <ul className="product-list">
-
+        {selectedProducts.map((product) => (
+            <li key={product.id} className="product-card">
+            <img
+                src={product.imageUrl}
+                alt={product.name}
+                width="300"
+                height="300"
+            />
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>{formatCurrency(product.price)}</p>
+            {isInCart(cartItems, product) ? (
+                <Link to="/cart">Added to cart</Link>
+            ) : (
+                <button onClick={() => handleAddToCart(product)}>
+                + Add to cart
+                </button>
+            )}
+            </li>
+        ))}
         </ul>
     );
 };
